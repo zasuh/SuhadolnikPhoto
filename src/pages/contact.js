1ac8b48615ac4b7b55fb0668a1789fd30cce22a3
Zane Suhadolnik <zanesuhi@gmail.com>
2019-10-02 17:42:47 +0200

    updated

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Layout } from '../components'
+
+const contact = () => {
+  return (
+    <Layout customSEO>
+    
+    </Layout>
+  )
+}
+
+
+export default contact
