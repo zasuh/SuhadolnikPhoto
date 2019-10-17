6b72ff2875585c0e40f38f3e1295063a462f7596
Zane Suhadolnik <zanesuhi@gmail.com>
2019-10-17 06:38:41 +0200

    [SP] Commit to test netlify function for sending email

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -2,24 +2,35 @@ const SparkPost = require('sparkpost')
 
 const client = new SparkPost(process.env.SPARKPOST)
 
+const successCode = '200'
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
+
 exports.handler = function(event, context, callback) {
-  console.log(event)
-  console.log(context)
+  const data = JSON.parse(event.body)
+  const { name, email, subject, message } = data
   client.transmissions
     .send({
       options: {
         sandbox: true,
       },
       content: {
-        from: 'testing@sparkpostbox.com',
-        subject: 'Hello, World!',
-        html: '<html><body><p>My cool email.</p></body></html>',
+        from: email,
+        subject,
+        html: message,
       },
       recipients: [{ address: 'zanesuhi@gmail.com' }],
     })
-    .then(data => {
+    .then(response => {
       console.log('Mail has been sent successfully!')
-      console.log(data)
+      callback(null, {
+        statusCode: successCode,
+        headers,
+        body: JSON.stringify(response),
+      })
+      console.log(response)
     })
     .catch(err => {
       console.log('Whoops! Something went wrong')
