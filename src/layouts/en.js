2d302bfb7ccec4c0a81d52ead4c8f298f1734a8f
Zane Suhadolnik <zanesuhi@gmail.com>
2019-11-25 07:03:40 +0100

    [SP] Updated

---
+++ src/layouts/en.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { addLocaleData } from 'react-intl'
+import en from 'react-intl/locale-data/en'
+import 'intl/locale-data/jsonp/en'
+
+import messages from '../data/messages/en'
+import Layout from '../components/index'
